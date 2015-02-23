55632882c800c5b7659da6a92b93cfacdb9eafe1
Toshiyuki Satou <satou.toshiyuki1994@gmail.com>
2015-02-23 18:54:02 +0900

    decompose main.py into module

--- disp_term.py
+++ disp_term.py
@@ -1,7 +1,13 @@
 from termcolor import color
 
+def clear_display():
+    print("\033[2J") # clear
+    print("\033[1H") # hidari ue
+
 def print_board(b, lxy):
     color_01 = {0:'clear', 1:'cyan'}
+    clear_display()
+
     for j in range(lxy[0]):
         for i in range(lxy[1]):
             print(color(color_01[b[i][j]], '  '), end='')
