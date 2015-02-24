1abf6ac40dcbea93be149047d155c4787472743e
Toshiyuki Satou <satou.toshiyuki1994@gmail.com>
2015-02-24 12:30:09 +0900

    fix

--- nbhd.py
+++ nbhd.py
@@ -1,16 +1,11 @@
 from functools import partial
 
 def moore(r):
-    def _moore(r):
-        nbhd_list = [(i,j) for i in range(-r, r+1) for j in range(-r, r+1)]
-        nbhd_list.remove((0, 0))
-        return nbhd_list
-
-    return partial(_moore, r)
+    nbhd_list = [(i,j) for i in range(-r, r+1) for j in range(-r, r+1)]
+    nbhd_list.remove((0, 0))
+    return lambda: nbhd_list
 
 def neumann(r):
-    def _neumann(r):
-        nbhd_list = [(i,j) for i in range(-r, r+1) for j in range(-r, r+1) if abs(i)+abs(j) <= r]
-        nbhd_list.remove((0, 0))
-        return nbhd_list
-    return partial(_neumann, r)
+    nbhd_list = [(i,j) for i in range(-r, r+1) for j in range(-r, r+1) if abs(i)+abs(j) <= r]
+    nbhd_list.remove((0, 0))
+    return lambda: nbhd_list
